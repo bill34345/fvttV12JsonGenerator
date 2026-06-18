a7f7327632fc7aade6d8ce098c08ab91c41a188c
zhengkaizhong <1179016514@qq.com>
2026-06-18 11:26:09 +0800

    Ignore local generated artifacts

--- test_i18n.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const dict = JSON.parse(fs.readFileSync('data/cn.json', 'utf-8'));
-const val = "心灵";
-let res = null;
-for(let k in dict) { if(dict[k] === val) res = k; }
-console.log(res);
